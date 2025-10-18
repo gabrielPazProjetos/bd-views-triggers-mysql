8c49115f10ab73867dacac85dad91006093a69b0
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-18 00:45:23 -0300

    Create consultas.sql

---
+++ consultas.sql
@@ -0,0 +1,10 @@
+-- Consultas para visualizar os resultados
+
+SELECT * FROM vw_empregados_por_depto_local;
+SELECT * FROM vw_departamentos_gerentes;
+SELECT * FROM vw_projetos_mais_empregados;
+SELECT * FROM vw_projetos_departamentos_gerentes;
+SELECT * FROM vw_empregados_dependentes_gerentes;
+
+SELECT * FROM Deleted_Employee;
+SELECT * FROM Historico_Salario;
